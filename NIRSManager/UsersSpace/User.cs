d48b70391e49f9704c450d80fe6636e18b15c025
Damian <Damian@DESKTOP-5O0T58H>
2018-03-20 14:34:06 +0300

    Изменен класс данных авторизации, добавлен класс пользователя

---
+++ NIRSManager/UsersSpace/User.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NIRSManager.UsersSpace
+{
+    internal sealed class User
+    {
+        #region Private
+        private string _name;
+        private string _surname;
+        private string _secondName;
+        private DateTime _dateOfBirth;
+        private string _position;
+        #endregion
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string SurName
+        {
+            get { return _surname; }
+            set { _surname = value; }
+        }
+        public string SecondName
+        {
+            get { return _secondName; }
+            set { _secondName = value; }
+        }
+        public DateTime DateOfBirth
+        {
+            get { return _dateOfBirth; }
+            set { _dateOfBirth = value; }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+
+    }
+}
---
+++ NIRSManager/UsersSpace/UserData.cs
@@ -0,0 +1,45 @@
+﻿namespace NIRSManager.UsersSpace
+{
+    /// <summary>
+    /// Класс предназначен для хранения пользовательских данных для определения соответствующих файлов настроек
+    /// </summary>
+    internal sealed class UserData
+    {
+        #region Private
+        private string _login;
+        private string _md5;
+        #endregion
+
+        /// <summary>
+        /// Метод возвращает логин пользователя в системе, который необходим для загрузки настроек пользователя
+        /// </summary>
+        /// <param name="md5">md5-хеш логина и пароля</param>
+        /// <returns></returns>
+        public string GetFileName(string md5)
+        {
+            if (md5 == _md5)
+                return _login;
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// Конструктор по умолчанию, без параметров
+        /// </summary>
+        public UserData()
+        {
+            _login = string.Empty;
+            _md5 = string.Empty;
+        }
+
+        /// <summary>
+        /// Переопределенный конструктор с параметрами
+        /// </summary>
+        /// <param name="login">Логин пользователя</param>
+        /// <param name="md5">md5-хеш логина и пароля</param>
+        public UserData(string login, string md5)
+        {
+            _login = login;
+            _md5 = md5;
+        }
+    }
+}
