6ac3a1df0395156acaeac70936b9f80bf3049eec
Damian <Damian@DESKTOP-5O0T58H>
2018-04-04 22:22:32 +0300

    Добавлены классы-обертки данных для регистрации на сервере

---
+++ NIRSCore/Syncronization/LoginData.cs
@@ -0,0 +1,24 @@
+﻿namespace NIRSCore.Syncronization
+{
+    /// <summary>
+    /// Специальный контейнер-обертка для обмена данными с сервером
+    /// </summary>
+    public sealed class LoginData
+    {
+        /// <summary>
+        /// Логин пользователя
+        /// </summary>
+        public string Login { get; set; }
+
+        /// <summary>
+        /// Стандартный конструктор
+        /// </summary>
+        public LoginData() => Login = string.Empty;
+
+        /// <summary>
+        /// Конструктор с параметром
+        /// </summary>
+        /// <param name="login">огин пользователя</param>
+        public LoginData(string login) => Login = login;
+    }
+}
---
+++ NIRSCore/Syncronization/RegistrationData.cs
@@ -0,0 +1,26 @@
+﻿namespace NIRSCore.Syncronization
+{
+    /// <summary>
+    /// Специальный контейнер-обертка для данных регистрации на сервере
+    /// </summary>
+    public sealed class RegistrationData
+    {
+        /// <summary>
+        /// Логин пользователя
+        /// </summary>
+        public string Login { get; set; }
+
+        /// <summary>
+        /// Md5-сумма логина и пароля
+        /// </summary>
+        public string Md5 { get; set; }
+
+        public RegistrationData() => Login = Md5 = string.Empty;
+
+        public RegistrationData(string login, string md5)
+        {
+            Login = login;
+            Md5 = md5;
+        }
+    }
+}
