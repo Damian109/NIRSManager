58bd1ea962bc99c4b32e3e2953494716f7fb1a67
Damian <Damian@DESKTOP-5O0T58H>
2018-04-06 15:38:50 +0300

    Добавлен класс под хранения элемента меню

---
+++ NIRSManagerClient/CoreModels/MainMenuItem.cs
@@ -0,0 +1,26 @@
+﻿using NIRSCore;
+
+namespace NIRSManagerClient.CoreModels
+{
+    /// <summary>
+    /// В объекте данного класса хранится информация о названии пункта меню а также модели представления
+    /// </summary>
+    public sealed class MainMenuItem
+    {
+        /// <summary>
+        /// Название
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Модель представления
+        /// </summary>
+        public ViewModel NewViewModel { get; set; }
+
+        public MainMenuItem(string name, ViewModel viewModel)
+        {
+            Name = name;
+            NewViewModel = viewModel;
+        }
+    }
+}
