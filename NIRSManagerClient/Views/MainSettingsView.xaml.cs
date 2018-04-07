2856f0e014fccdd43d8d73733bf5b158d032eb07
Damian <Damian@DESKTOP-5O0T58H>
2018-04-07 14:19:37 +0300

    Добавлено представление для Основных настроек

---
+++ NIRSManagerClient/Views/MainSettingsView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using NIRSCore;
+using System.Windows.Controls;
+using NIRSManagerClient.ViewModels.SettingsViewModels;
+
+namespace NIRSManagerClient.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для MainSettingsView.xaml
+    /// </summary>
+    public partial class MainSettingsView : UserControl
+    {
+        public MainSettingsView(User user)
+        {
+            InitializeComponent();
+            DataContext = new MainSettingsViewModel(user);
+        }
+    }
+}
