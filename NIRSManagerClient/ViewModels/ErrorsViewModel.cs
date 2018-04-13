5a880d974ddf30cc5842b4c0685a6e48a36805d7
Damian <Damian@DESKTOP-5O0T58H>
2018-04-13 23:59:08 +0300

    Добавлено представление диспетчера ошибок

---
+++ NIRSManagerClient/ViewModels/ErrorsViewModel.cs
@@ -0,0 +1,44 @@
+﻿using NIRSCore;
+using NIRSCore.ErrorManager;
+using System.Collections.Generic;
+
+namespace NIRSManagerClient.ViewModels
+{
+    public sealed class ErrorsViewModel : ViewModel
+    {
+        #region Private
+        /// <summary>
+        /// Получение списка ошибок
+        /// </summary>
+        private void GetErrors()
+        {
+            Errors = NirsSystem.ErrorManager.GetErrors();
+            OnPropertyChanged("Errors");
+        }
+
+        #endregion
+
+        public ErrorsViewModel() : base("Главная форма") { }
+
+        /// <summary>
+        /// Список ошибок
+        /// </summary>
+        public List<NirsError> Errors { get; set; }
+
+        /// <summary>
+        /// Команда Отправить на сервер
+        /// </summary>
+        public RelayCommand CommandSetToServer
+        {
+            get => new RelayCommand(obj => NirsSystem.ErrorManager.SetToServer());
+        }
+
+        /// <summary>
+        /// Команда Очистить
+        /// </summary>
+        public RelayCommand CommandClear
+        {
+            get => new RelayCommand(obj => NirsSystem.ErrorManager.Clear());
+        }
+    }
+}
---
+++ NIRSManagerClient/Views/ErrorsView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+using NIRSManagerClient.ViewModels;
+
+namespace NIRSManagerClient.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для ErrorsView.xaml
+    /// </summary>
+    public partial class ErrorsView : UserControl
+    {
+        public ErrorsView()
+        {
+            InitializeComponent();
+            DataContext = new ErrorsViewModel();
+        }
+    }
+}
