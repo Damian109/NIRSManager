1a7bffea548b629694ea19fa06c1d5c25c6f9082
Damian <Damian@DESKTOP-5O0T58H>
2018-04-14 11:45:42 +0300

    Добавления ряда представлений для прототипа

---
+++ NIRSManagerClient/Views/BackupView.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NIRSManagerClient.Views
+{
+    public class TR
+    {
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+
+        public TR(string name, DateTime date)
+        {
+            Name = name;
+            Date = date;
+        }
+    }
+
+    /// <summary>
+    /// Логика взаимодействия для BackupView.xaml
+    /// </summary>
+    public partial class BackupView : UserControl
+    {
+        public BackupView()
+        {
+            InitializeComponent();
+
+            Backups = new List<TR>
+            {
+                new TR("backup-13-04-2018", DateTime.Now),
+                new TR("backup-13-04-2018", DateTime.Now),
+                new TR("backup-13-04-2018", DateTime.Now),
+                new TR("backup-13-04-2018", DateTime.Now),
+            };
+
+            t.ItemsSource = Backups;
+        }
+
+        public List<TR> Backups { get; set; }
+    }
+}
---
+++ NIRSManagerClient/Views/ExchangeErrorView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NIRSManagerClient.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для ExchangeErrorView.xaml
+    /// </summary>
+    public partial class ExchangeErrorView : UserControl
+    {
+        public ExchangeErrorView()
+        {
+            InitializeComponent();
+        }
+    }
+}
