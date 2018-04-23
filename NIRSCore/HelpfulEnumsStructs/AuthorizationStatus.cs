567540b516b83cf822c231409297ac1b56157b74
Damian <Damian@DESKTOP-5O0T58H>
2018-04-23 18:02:37 +0300

    Добавление перечисления для авторизации, пакета для удобной работы с сервером

---
+++ NIRSCore/HelpfulEnumsStructs/AuthorizationStatus.cs
@@ -0,0 +1,13 @@
+﻿namespace NIRSCore.HelpfulEnumsStructs
+{
+    /// <summary>
+    /// Перечисление, описывающее статус авторизации
+    /// </summary>
+    public enum AuthorizationStatus
+    {
+        AuthLogin,     //Необходимо ввести логин
+        AuthPassword,  //Необходимо ввести пароль
+        AuthOK,        //Все данные введены
+        AuthError      //Авторизация не удалась
+    }
+}
