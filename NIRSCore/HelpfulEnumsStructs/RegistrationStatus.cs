d345b82f50ca6668249a0a4116f71069d31cb7bf
Damian <Damian@DESKTOP-5O0T58H>
2018-04-23 18:49:12 +0300

    добавлено перечисление для работы со статусом регистрации

---
+++ NIRSCore/HelpfulEnumsStructs/RegistrationStatus.cs
@@ -0,0 +1,15 @@
+﻿namespace NIRSCore.HelpfulEnumsStructs
+{
+    /// <summary>
+    /// Перечисление, описывающее статус регистрации
+    /// </summary>
+    enum RegistrationStatus
+    {
+        RegLogin,     //Ввести логин
+        RegPassword,  //Ввести пароль
+        RegOK,        //Данные корректны
+        RegError,     //Такой пользователь существует
+        RegServerErr, //Сервер недоступен
+        RegGood       //Регистрация успешна
+    }
+}
