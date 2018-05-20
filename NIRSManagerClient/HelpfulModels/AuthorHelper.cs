7dc3a4ee2e967ca990f9331bb20ea3308ea5038e
Damian <Damian@DESKTOP-5O0T58H>
2018-05-20 22:52:08 +0300

    Исправлен элемент для отображения в таблице авторов

--- NIRSManagerClient/HelpfulModels/AuthorHelper.cs
+++ NIRSManagerClient/HelpfulModels/AuthorHelper.cs
@@ -1,6 +1,5 @@
 ﻿using NIRSCore;
 using NIRSCore.DataBaseModels;
-using System;
 
 namespace NIRSManagerClient.HelpfulModels
 {
@@ -9,37 +8,75 @@ namespace NIRSManagerClient.HelpfulModels
         /// <summary>
         /// Идентификатор автора
         /// </summary>
-        public int UserId { get; private set; }
+        public int AuthorId { get; private set; }
 
         /// <summary>
         /// ФИО Автора
         /// </summary>
-        public string Fio { get; private set; }
-
-        /// <summary>
-        /// Должность
-        /// </summary>
-        public string Position { get; private set; }
-
-        /// <summary>
-        /// Название организации
-        /// </summary>
-        public string Organization { get; private set; }
+        public string AuthorName { get; private set; }
 
         /// <summary>
         /// Путь к личному фото
         /// </summary>
-        public string PathPhoto { get; private set; }
+        public string PhotoPath { get; private set; }
 
+        /// <summary>
+        /// Организация
+        /// </summary>
+        public string OrganizationName { get; set; }
 
-        public AuthorHelper(int id)
+        /// <summary>
+        /// Факультет
+        /// </summary>
+        public string FacultyName { get; set; }
+
+        /// <summary>
+        /// Кафедра
+        /// </summary>
+        public string DepartmentName { get; set; }
+
+        /// <summary>
+        /// Группа
+        /// </summary>
+        public string GroupName { get; set; }
+
+        /// <summary>
+        /// Должность
+        /// </summary>
+        public string PositionName { get; set; }
+
+        /// <summary>
+        /// Ученая степень
+        /// </summary>
+        public string AcademicDegreeName { get; set; }
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="author">Автор</param>
+        public AuthorHelper(Author author)
         {
-            UserId = id;
-            Author authorQuery = NirsSystem.GetAuthor(id);
-            Fio = authorQuery.SurName + " " + authorQuery.Name + " " + authorQuery.SecondName;
-            PathPhoto = Environment.CurrentDirectory + authorQuery.PathPhoto;
-            Position = NirsSystem.GetPosition(authorQuery.PositionId)?.PositionName;
-            Organization = NirsSystem.GetOrganization(authorQuery.OrganizationId)?.OrganizationName;
+            AuthorId = author.AuthorId;
+            AuthorName = author.AuthorName;
+            PhotoPath = author.PhotoPath;
+
+            Organization organization = (Organization)NirsSystem.GetObject<Organization>(author.OrganizationId);
+            OrganizationName = organization.OrganizationName;
+
+            Faculty faculty = (Faculty)NirsSystem.GetObject<Faculty>((int)author.FacultyId);
+            FacultyName = "Факультет: " + faculty.FacultyName;
+
+            Department department = (Department)NirsSystem.GetObject<Department>((int)author.DepartmentId);
+            DepartmentName = "Кафедра: " + department.DepartmentName;
+
+            Group group = (Group)NirsSystem.GetObject<Group>((int)author.GroupId);
+            GroupName = "Группа: " + group.GroupName;
+
+            Position position = (Position)NirsSystem.GetObject<Position>((int)author.PositionId);
+            PositionName = "Должность: " + position.PositionName;
+
+            AcademicDegree academicDegree = (AcademicDegree)NirsSystem.GetObject<AcademicDegree>((int)author.AcademicDegreeId);
+            AcademicDegreeName = academicDegree.AcademicDegreeName;
         }
     }
 }
