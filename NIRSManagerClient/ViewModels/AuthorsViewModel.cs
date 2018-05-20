0bb4f2b879267573a5118e208656a983a8dd15db
Damian <Damian@DESKTOP-5O0T58H>
2018-05-21 00:46:24 +0300

    Изменена страница авторов

--- NIRSManagerClient/ViewModels/AuthorsViewModel.cs
+++ NIRSManagerClient/ViewModels/AuthorsViewModel.cs
@@ -2,18 +2,25 @@
 using System.Linq;
 using System.Windows;
 using System.Threading.Tasks;
+using NIRSCore.DataBaseModels;
 using NIRSManagerClient.Views;
 using System.Collections.Generic;
 using NIRSManagerClient.HelpfulModels;
-
 
 namespace NIRSManagerClient.ViewModels
 {
     public sealed class AuthorsViewModel : ViewModel
     {
-        //Первоначальный список авторов
-        private List<AuthorHelper> _authors;
+        //Полный список авторов
+        private List<Author> _authorsFull;
+
+        //Строка поиска. Остается на все время сессии
         private static string _search = string.Empty;
+
+        /// <summary>
+        /// Базовый конструктор
+        /// </summary>
+        public AuthorsViewModel() : base("Авторы") => GetAuthors();
 
         /// <summary>
         /// Строка поиска
@@ -23,6 +30,40 @@ namespace NIRSManagerClient.ViewModels
             get => _search;
             set => _search = value;
         }
+
+        /// <summary>
+        /// Список авторов
+        /// </summary>
+        public List<AuthorHelper> Authors { get; private set; }
+
+        /// <summary>
+        /// Получение списка авторов с учетом правил поиска
+        /// </summary>
+        private async void GetAuthors() => await Task.Run(() =>
+        {
+            Authors = new List<AuthorHelper>();
+            _authorsFull = (List<Author>)NirsSystem.GetListObject<Author>();
+            if (_authorsFull == null || _authorsFull.Count < 1)
+                return;
+            if (_search == string.Empty)
+            {
+                foreach (var elem in _authorsFull)
+                    Authors.Add(new AuthorHelper(elem));
+                OnPropertyChanged("Authors");
+                return;
+            }
+            if (_search[0] == '#')
+            {
+
+            }
+            else
+            {
+                foreach (var elem in _authorsFull)
+                    if (elem.AuthorName.Contains(_search))
+                        Authors.Add(new AuthorHelper(elem));
+            }
+            OnPropertyChanged("Authors");
+        });
 
         /// <summary>
         /// Команда - Добавить автора
@@ -42,77 +83,7 @@ namespace NIRSManagerClient.ViewModels
         /// </summary>
         public RelayCommand CommandSearch
         {
-            get => new RelayCommand(obj => FillAuthors());
-        }
-
-        /// <summary>
-        /// Базовый конструктор
-        /// </summary>
-        public AuthorsViewModel() : base("Авторы")
-        {
-            _authors = new List<AuthorHelper>();
-            FillAuthors();
-        }
-
-        /// <summary>
-        /// Список авторов
-        /// </summary>
-        public List<AuthorHelper> Authors { get => _authors; }
-
-        /// <summary>
-        /// Заполнение списка авторов
-        /// </summary>
-        private void FillAuthors()
-        {
-            GetAuthors();
-            if (_authors == null)
-                _authors = new List<AuthorHelper>();
-            OnPropertyChanged("Authors");
-        }
-
-        /// <summary>
-        /// Получение списка авторов с учетом правил поиска
-        /// </summary>
-        private async void GetAuthors()
-        {
-            await Task.Run(() =>
-            {
-                List<AuthorHelper> queryResult = new List<AuthorHelper>();
-                List<AuthorHelper> result = new List<AuthorHelper>();
-                var query = NirsSystem.GetAuthors();
-                if (query != null)
-                    foreach (var elem in query)
-                        queryResult.Add(new AuthorHelper(elem.UserId));
-                foreach (var elem in queryResult)
-                {
-                    if (elem.Fio != null)
-                    {
-                        if (elem.Fio.Contains(_search))
-                        {
-                            result.Add(elem);
-                            continue;
-                        }
-                    }
-                    if (elem.Position != null)
-                    {
-                        if (elem.Position.Contains(_search))
-                        {
-                            result.Add(elem);
-                            continue;
-                        }
-                    }
-                    if (elem.Organization != null)
-                    {
-                        if (elem.Organization.Contains(_search))
-                        {
-                            result.Add(elem);
-                            continue;
-                        }
-                    }
-                }
-                _authors = result;
-                OnPropertyChanged("Authors");
-            });
+            get => new RelayCommand(obj => GetAuthors());
         }
     }
 }
