ac750c89019c60c3c5ccabccb03100ba3fe996ea
Damian <Damian@DESKTOP-5O0T58H>
2018-05-25 15:19:42 +0300

    Добавлено представление стека операций

---
+++ NIRSManagerClient/Views/StackView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+using NIRSManagerClient.ViewModels;
+
+namespace NIRSManagerClient.Views
+{
+    public partial class StackView : UserControl
+    {
+        public StackView()
+        {
+            InitializeComponent();
+            DataContext = new StackViewModel();
+        }
+    }
+}
