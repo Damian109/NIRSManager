880aff6b9d5d471bc329f526bceb1d4d0b9b4fd9
Damian <Damian@DESKTOP-5O0T58H>
2018-05-26 00:03:21 +0300

    Добавлено представление страницы отчетов

---
+++ NIRSManagerClient/Views/ReportView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+using NIRSManagerClient.HelpfulModels;
+using NIRSManagerClient.ViewModels;
+
+namespace NIRSManagerClient.Views
+{
+    public partial class ReportView : UserControl
+    {
+        public ReportView(List<AuthorHelper> authorHelpers, int filter, string search)
+        {
+            InitializeComponent();
+            DataContext = new ReportViewModel(authorHelpers, filter, search);
+        }
+    }
+}
