d803e477d5f7c860d32733b1ca24fd33689ddc9d
Damian <Damian@DESKTOP-5O0T58H>
2018-06-02 20:48:47 +0300

    Добавлены ссылки на страницу о программе

--- NIRSManagerClient/Views/AboutView.xaml.cs
+++ NIRSManagerClient/Views/AboutView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Diagnostics;
+using System.Windows.Controls;
 
 namespace NIRSManagerClient.Views
 {
@@ -7,6 +8,21 @@ namespace NIRSManagerClient.Views
         public AboutView()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Process.Start("https://github.com/Damian109");
+        }
+
+        private void Button_Click_1(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Process.Start("https://vk.com/damian01");
+        }
+
+        private void Button_Click_2(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Process.Start("mailto://damian25091995@gmail.com");
         }
     }
 }
