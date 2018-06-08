2bc89aec27abdb426e295475f81bc989ce5935ff
Damian <Damian@DESKTOP-5O0T58H>
2018-06-08 19:29:58 +0300

    Добавле менеджер резервных копий

---
+++ NIRSCore/BackupManager/BackupElem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NIRSCore.BackupManager
+{
+    /// <summary>
+    /// Описание резервной копии
+    /// </summary>
+    [Serializable]
+    public sealed class BackupElem
+    {
+        /// <summary>
+        /// Название резервной копии
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// СУБД
+        /// </summary>
+        public string DBMSName { get; set; }
+
+        /// <summary>
+        /// Дата создания
+        /// </summary>
+        public DateTime DateOfCreate { get; set; }
+    }
+}
---
+++ NIRSCore/BackupManager/BackupManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using NIRSCore.Syncronization;
+
+namespace NIRSCore.BackupManager
+{
+    public sealed class BackupManager
+    {/*
+        public async void GetFileBackupFromServerAsync(LoginData loginData, string name, string adress) => await Task.Run(async () =>
+        {
+            using (var client = new HttpClient())
+            {
+                //Формирование строки запроса
+                string query
+
+                ///HttpResponseMessage message = client.GetAsync(adress + "Sync/GetFile?LoginData=" + login).Result;
+                string path = Environment.CurrentDirectory + "\\data\\" + NirsSystem.GetLogin() + "\\temp\\";
+                using (FileStream file = new FileStream(path + name, FileMode.Create))
+                {
+                    await message.Content.CopyToAsync(file);
+                }
+            }
+        });*/
+    }
+}
