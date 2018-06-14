ae797b05497dceba42f2ca60e2b4fe1b9fba7380
Damian <Damian@DESKTOP-5O0T58H>
2018-06-14 13:43:39 +0300

    Добавлены элементы для обмена данными между клиентом и сервером во время обмена БД

---
+++ NIRSCore/Syncronization/ListExchangesData.cs
@@ -0,0 +1,43 @@
+﻿namespace NIRSCore.Syncronization
+{
+    /// <summary>
+    /// Объект представляет список всех обменов, предложенных пользователю или пользователем
+    /// </summary>
+    public sealed class ListExchangesData
+    {
+        /// <summary>
+        /// Идентификатор обмена
+        /// </summary>
+        public int ExchangeId { get; set; }
+
+        /// <summary>
+        /// Логин получателя или отправителя
+        /// </summary>
+        public string LoginCreatorOrSender { get; set; }
+
+        /// <summary>
+        /// Является ли текущий пользователь создателем обмена
+        /// </summary>
+        public bool IsIAmCreator { get; set; }
+
+        /// <summary>
+        /// Односторонний ли обмен
+        /// </summary>
+        public bool IsOneWay { get; set; }
+
+        /// <summary>
+        /// Принял ли получатель обмен
+        /// </summary>
+        public bool IsSenderAccept { get; set; }
+
+        /// <summary>
+        /// Выполнил ли создатель обмен
+        /// </summary>
+        public bool IsCreatorDone { get; set; }
+
+        /// <summary>
+        /// Выполнил ли получатель обмен
+        /// </summary>
+        public bool IsSenderDone { get; set; }
+    }
+}
---
+++ NIRSCore/Syncronization/ListLoginsData.cs
@@ -0,0 +1,14 @@
+﻿namespace NIRSCore.Syncronization
+{
+    /// <summary>
+    /// Объект представляет список всех доступных для обмена пользователей
+    /// </summary>
+    public sealed class ListLoginsData
+    {
+        public string[] ListLogins { get; set; }
+
+        public ListLoginsData() => ListLogins = null;
+
+        public ListLoginsData(string[] strings) => ListLogins = strings;
+    }
+}
